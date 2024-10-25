d2cf419432f75ef6724e7bcb0b961110323cd894
volkoluck74 <volko-ov@yandex.ru>
2024-10-25 17:41:29 +0000

    change README.md and delete useless function

--- README.md
+++ README.md
@@ -15,9 +15,13 @@ brain-progression //In this game we are looking for the missing element of the p
 brain-prime //In this game, we determine whether a number is prime.
 ```
 [brain-even](https://asciinema.org/a/ByNA8sATYo9SFA1tvp1fJPx)
+
 [brain-calc](https://asciinema.org/a/GlT8YsNaldxIAJSprcFA3n5gi)
+
 [brain-gcd](https://asciinema.org/a/JYjVAalMudAApcYROBnGPEvwS)
+
 [brain-progression](https://asciinema.org/a/pk3IdphD43jQtPGC5fVClRg0N)
+
 [brain-prime](https://asciinema.org/a/olrtUabMv1qDruvFru5ahF7Rq)
 
 [![Actions Status](https://github.com/volkoluck74/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/volkoluck74/frontend-project-44/actions)
--- src/utils/mathAndLogic.js
+++ src/utils/mathAndLogic.js
@@ -63,29 +63,3 @@ export function isPrime(number) {
   }
   return 'yes';
 }
-// Выводим арифмитическую програссию и возвращаем ее скрытый элемент
-export function getHiddenElement() {
-  // Длина прогрессии от 5 до 10 чисел
-  let lengthProgression = createRandomNumberBefore10();
-  if (lengthProgression < 5) lengthProgression = 10 - lengthProgression;
-  // Шаг прогрессии
-  const step = createRandomNumberBefore10();
-  // Номер скрытого элемента
-  let numberOfHiddenElement = createRandomNumberBefore10();
-  // Первый элемент прогрессии
-  const firstElement = createRandomNumberBefore10();
-  if (numberOfHiddenElement > lengthProgression) numberOfHiddenElement -= lengthProgression;
-  const progression = [];
-  for (let i = 0; i < lengthProgression; i += 1) {
-    if (i !== numberOfHiddenElement - 1) progression.push(firstElement + step * i);
-    else progression.push('..');
-  }
-  // Значение скрытого элемента
-  const answer = firstElement + step * (numberOfHiddenElement - 1);
-  let question = '';
-  for (let i = 0; i < progression.length; i += 1) {
-    question += `${progression[i]} `;
-  }
-  console.log(`Question: ${question}`);
-  return answer.toString();
-}

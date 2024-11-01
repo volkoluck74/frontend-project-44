c66090a0485e3e672b102ee73f0a771b60af5536
volkoluck74 <volko-ov@yandex.ru>
2024-11-01 14:30:45 +0000

    add choose minValue in randomNumber

--- src/utils/createRandomNumber.js
+++ src/utils/createRandomNumber.js
@@ -1,4 +1,6 @@
-// Создаем случайное число от 1 до maxNumber
-export default function createRandomNumber(maxNumber) {
-  return Math.ceil(Math.random() * maxNumber);
+// Создаем случайное число от minNumber до maxNumber
+export default function createRandomNumber(maxNumber, minNumber = 1) {
+  const result = Math.ceil(Math.random() * maxNumber);
+  if (result < minNumber) return createRandomNumber();
+  return result;
 }
